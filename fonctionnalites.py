a63fd9550a697f8218d752a6b47d05518395c4e9
RaoufAddeche <addeche.raouf@gmail.com>
2024-10-23 13:41:28 +0200

    fonctionnalité multiplication

---
+++ fonctionnalites.py
@@ -0,0 +1,15 @@
+def multiplication(a : int,b : int) -> int :
+
+    """Multiply two numbers.
+
+    This function takes two numerical inputs and returns their product. It is a simple utility for performing multiplication.
+
+    Args:
+        a (int or float): The first number to multiply.
+        b (int or float): The second number to multiply.
+
+    Returns:
+        int or float: The product of the two input numbers.
+    """
+
+    return a * b
